ccd72eaa340c1b7678895050fbf9f22030906966
Alex D <alex8d@pm.me>
2023-01-23 16:17:45 +0100

    remove some generated text

--- cmd/dl.go
+++ cmd/dl.go
@@ -35,12 +35,6 @@ import (
 var dlCmd = &cobra.Command{
 	Use:   "dl",
 	Short: "Download and save all activities of a user",
-	//	Long: `A longer description that spans multiple lines and likely contains examples
-	//and usage of using your command. For example:
-	//
-	//Cobra is a CLI library for Go that empowers applications.
-	//This application is a tool to generate the needed files
-	//to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		downloadAndSave()
 	},
@@ -80,14 +74,4 @@ func downloadAndSave() {
 
 func init() {
 	rootCmd.AddCommand(dlCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// dlCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// dlCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
